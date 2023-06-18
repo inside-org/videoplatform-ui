26d4bba78da4a4079529f8f8e9c090d8a070be3e
Боровский Дмитрий <DBorovskiy@alfabank.ru>
2023-06-18 14:08:49 +0300

    feaure(*): add form AuthForm

--- src/components/pages/profile/index.tsx
+++ src/components/pages/profile/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AuthForm from 'Src/forms/auth-form'
 
 import styles from './styles.module.css'
 
@@ -6,6 +7,7 @@ const Profile = () => {
     return (
         <div className={styles.profile}>
             Profile page
+            <AuthForm />
         </div>
     );
 }
---
+++ src/forms/auth-form/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+
+import styles from './styles.module.css'
+
+const AuthForm = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(event.target.value);
+    };
+
+    const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
+    return (
+        <Box component="form" className={styles.auth}>
+            <FormControl className={styles.auth-item}>
+                <TextField
+                    value={ login }
+                    onChange={ handleChangeLogin }
+                    id="input-login"
+                    label="Логин"
+                    variant="outlined"
+                />
+            </FormControl>
+            <FormControl>
+                <TextField
+                    value={ password }
+                    onChange={ handleChangePassword }
+                    id="input-password"
+                    label="Пароль"
+                    variant="outlined"
+                    type="password"
+                />
+            </FormControl>
+        </Box>
+    );
+}
+
+export default AuthForm;
